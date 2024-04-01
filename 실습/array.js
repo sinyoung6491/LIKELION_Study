f571ac25b0b5cb7a922f27c4acc53f644e1c5a08
sinyoung6491 <sinyoung6491@gmail.com>
2024-04-01 23:51:36 +0900

    week01_commit

---
+++ 실습/array.js
@@ -0,0 +1,21 @@
+const arr = [100,[200,300],["jacascript","jquery"]];
+
+console.log(arr[0]);
+console.log(arr[1][0]);
+console.log(arr[1][1]);
+console.log(arr[2][0]);
+console.log(arr[2][1]);
+
+const num = 100;
+
+(num == 100)?document.write("true"):document.write(false);
+
+switch(num){
+    case 90:
+        document.write("실행되었습니다.()");
+        break;
+    case 95:
+        document.write("실행되었습니다.()");
+    case 100:
+        document
+}
